02d2e3194c442d8c32d9e731a7d9e8eefcb9a606
Dipendra6377 <91822875+Dipendra6377@users.noreply.github.com>
2022-12-27 12:41:56 +0530

    Update 03_check_if_Array_is_Sorted_or_Rotated.java

--- 26-December-2022/Question 1 to/03_check_if_Array_is_Sorted_or_Rotated.java
+++ 26-December-2022/Question 1 to/03_check_if_Array_is_Sorted_or_Rotated.java
@@ -1,3 +1,5 @@
+//Best solution
+
 class Solution {
     public boolean check(int[] nums) {
         boolean arrayDecrease = false;
@@ -14,4 +16,21 @@ class Solution {
         }
         return !arrayDecrease || nums[nums.length-1]<=nums[0];
     }
+}
+
+//Altenate solution
+
+class Solution {
+	public boolean check(int[] a) {
+		 int c=0;
+		 if(a[0]<a[a.length-1])c++;
+		for(int i=0;i<a.length-1;i++){
+			if(a[i]>a[i+1])c++;
+
+		}
+
+
+		return c<=1;
+	}
+
 }
