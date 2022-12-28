287e7cc37279988b098136d0e9e50f66652f658f
Dipendra6377 <91822875+Dipendra6377@users.noreply.github.com>
2022-12-28 09:10:58 +0530

    Create 09_Maximum_Consecutive_Sum.java

---
+++ 26-December-2022/Question 1 to/09_Maximum_Consecutive_Sum.java
@@ -0,0 +1,40 @@
+//My solution 
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+            }
+            max=Math.max(count,max);
+        }
+        return max;
+    }
+}
+
+//Alternate Solution (Better solution)
+
+class Solution {
+  public int findMaxConsecutiveOnes(int[] nums) {
+    int count = 0;
+    int maxCount = 0;
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] == 1) {
+        // Increment the count of 1's by one.
+        count += 1;
+      } else {
+        // Find the maximum till now.
+        maxCount = Math.max(maxCount, count);
+        // Reset count of 1.
+        count = 0;
+      }
+    }
+    return Math.max(maxCount, count);
+  }
+}
